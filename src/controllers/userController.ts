7f02f9ea9d365f9d70076141c0fc207941fb243d
iohanasalvador <iohanasalvador412@gmail.com>
2025-04-27 19:32:41 -0300

    feat: getUser and getAll

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { CreateUserDTO } from '../dtos/createUserDTO'; 
-import { User } from '../models/user'; 
+import { User } from '../models/User'
 
 let users: User[] = [];
 let nextId = 1;
@@ -21,5 +21,19 @@ export class UserController {
 
     res.status(201).json(newUser);
   }
+  
+  static getAll(req: Request, res: Response): void {
+    res.json(users);
+  }
 
+  static getUser(req: Request, res: Response): void {
+    const id = Number(req.params.id);
+    const user = users.find(u => u.id === id);
+
+    if (!user) {
+      res.status(404).json({ message: 'Usuário não encontrado' });
+    } else {
+      res.json(user);
+    }
+  }
 }
