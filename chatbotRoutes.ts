3cb8e2e4130f29da3cc7ff2322a9d72800ffdfc0
Rodrigo Campos Cabral <142432355+Rodcamppos@users.noreply.github.com>
2025-04-28 20:16:47 -0300

    Update chatbotRoutes.ts

--- chatbotRoutes.ts
+++ chatbotRoutes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { handleUserMessage } from '../controllers/chatbotController';
 
+// Só modelando o código, por enquanto.
+
 const router = Router();
 
 // Rota para receber mensagens do usuário
