ef3bf2f5e866439daff3d9b431cd94710e1fd3c9
Rodrigo Campos Cabral <142432355+Rodcamppos@users.noreply.github.com>
2025-05-12 14:32:55 -0300

    Create dados.py

---
+++ src/dados.py
@@ -0,0 +1,20 @@
+import requests
+from utils import get_token
+
+def acessar_dados_protegidos():
+    token = get_token()
+    if not token:
+        return
+
+    headers = {'Authorization': f'Bearer {token}'}
+    url = ''
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        print('Dados protegidos:', response.json())
+    else:
+        print('Erro ao acessar dados:', response.status_code)
+
+if __name__ == '__main__':
+    acessar_dados_protegidos()
